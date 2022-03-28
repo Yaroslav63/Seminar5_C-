36264b5dded3208c8ba188ab351eb60ec4f4672e
Yaroslav63 <era6300@mail.ru>
2022-03-28 18:23:05 +0300

    add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿int [] CreateArray (int length)
+{
+    int[] array = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array [i] = new Random().Next(-200, 201);
+    }
+    return array;
+}
+
+int[] FindSumNumber (int[] array)
+{
+    int[] sumNumber = new int[array.Length];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(sumNumber[i]%2 == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            sum = sumNumber[i]++;
+        }
+    }
+    return sumNumber;
+}
+
+int[] arr = CreateArray(7);
+int [] res = FindSumNumber(arr);
+System.Console.WriteLine(res);
