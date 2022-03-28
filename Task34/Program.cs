0829015dd14eeec8086520a18fac9e204cea1deb
Yaroslav63 <era6300@mail.ru>
2022-03-28 18:22:56 +0300

    add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(99, 1000);
+    }
+    return array;
+}
+
+int[] GetEvenNumber (int[] array)
+{
+    int[] evenNumber = new int[array.Length];
+    int even_sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (evenNumber[i] % 2 == 0)
+        {
+            even_sum++;
+        }
+    }
+    return evenNumber;
+}
+int[] arr = CreateArray(1);
+GetEvenNumber(arr);
+int[] sum = GetEvenNumber(arr);
+Console.WriteLine(sum);
